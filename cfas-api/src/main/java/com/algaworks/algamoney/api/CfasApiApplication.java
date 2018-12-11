9f131cee33865e05521e302c05694bf83860fa28
Davi <davimss@gmail.com>
2018-12-11 17:23:02 -0300

    First commit

---
+++ cfas-api/src/main/java/com/algaworks/algamoney/api/CfasApiApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.algamoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfasApiApplication.class, args);
+	}
+}
