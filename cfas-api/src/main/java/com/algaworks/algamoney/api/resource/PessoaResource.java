7d88dca5e997df04ad54c5ed7066a56d30f98804
Davi <davimss@gmail.com>
2018-12-16 00:25:27 -0300

    Aula 4.1 completada

--- cfas-api/src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
+++ cfas-api/src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,12 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo, HttpServletResponse response) {
 		Optional<Pessoa> c = pessoaRepository.findById(codigo);		
 		return c.isPresent() ? ResponseEntity.ok(c.get()) : ResponseEntity.notFound().build(); 				
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.deleteById(codigo);
 	}
 
 }
