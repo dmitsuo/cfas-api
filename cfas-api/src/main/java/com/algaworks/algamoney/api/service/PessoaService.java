dd7b19f195e52a226fa30a07ca27c0b1f9f9fe55
Davi <davimss@gmail.com>
2018-12-17 10:47:23 -0300

    Aula 4.3 completada

---
+++ cfas-api/src/main/java/com/algaworks/algamoney/api/service/PessoaService.java
@@ -0,0 +1,28 @@
+package com.algaworks.algamoney.api.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algamoney.api.model.Pessoa;
+import com.algaworks.algamoney.api.repository.PessoaRepository;
+
+@Service
+public class PessoaService {
+	@Autowired
+	private PessoaRepository pessoaRepository;	
+	
+	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
+		Optional<Pessoa> tempObj = pessoaRepository.findById(codigo);
+		if (!tempObj.isPresent()) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		Pessoa pessoaSalva = tempObj.get();
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+
+}
