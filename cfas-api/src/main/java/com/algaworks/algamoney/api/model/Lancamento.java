a2f5b21c516178bd6deac3da95cef0f3fecc3313
davimss <davimss@gmail.com>
2018-12-23 23:34:24 -0300

    Aula 5.5 completada

--- cfas-api/src/main/java/com/algaworks/algamoney/api/model/Lancamento.java
+++ cfas-api/src/main/java/com/algaworks/algamoney/api/model/Lancamento.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -28,23 +29,29 @@ public class Lancamento {
 	@EqualsAndHashCode.Include
 	private Long codigo;
 	
+	@NotNull
 	private String descricao;
 
+	@NotNull
 	@Column(name = "data_vencimento")
 	private LocalDate dataVencimento;
 
 	@Column(name = "data_pagamento")
 	private LocalDate dataPagamento;
 
+	@NotNull
 	private BigDecimal valor;
 
+	@NotNull
 	@Enumerated(EnumType.STRING)
 	private TipoLancamento tipo;
 
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_categoria")
 	private Categoria categoria;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "codigo_pessoa")
 	private Pessoa pessoa;	
