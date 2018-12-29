818a014e02efa62def627a5a9063da49e4a033ce
Davi <davimss@gmail.com>
2018-12-29 03:26:30 -0300

    Aula 5.7 concluída.

--- src/main/java/com/algaworks/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -8,13 +8,13 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -96,12 +96,12 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		
 	}
 	
-//	@ExceptionHandler({BindException.class})	
-//	public ResponseEntity<Object> handleBindException(BindException ex, WebRequest request) {
-//		String mensagemUsuario = messageSource.getMessage("recurso.erro-conversao", null, LocaleContextHolder.getLocale());
-//		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
-//		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor)); 
-//		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
-//		
-//	}
+	@ExceptionHandler({ConversionFailedException.class})	
+	public ResponseEntity<Object> handleConversionFailedException(ConversionFailedException ex, WebRequest request) {
+		String mensagemUsuario = messageSource.getMessage("recurso.erro-conversao", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor)); 
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+		
+	}
 }
