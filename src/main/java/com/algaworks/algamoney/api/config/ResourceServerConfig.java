5d2eb6134c73cfd7a7a1a594396a5217a05a125a
Davi <davimss@gmail.com>
2019-07-16 12:00:34 -0300

    Aula 7.4 concluída. Liberado o path do H2 Console.

--- src/main/java/com/algaworks/algamoney/api/config/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/ResourceServerConfig.java
@@ -21,6 +21,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 		http.authorizeRequests()
 				.antMatchers("/categorias").permitAll()
+				.antMatchers("/h2-console").permitAll()
 				.antMatchers("/h2console").permitAll()
 				.anyRequest().authenticated()
 				.and()
