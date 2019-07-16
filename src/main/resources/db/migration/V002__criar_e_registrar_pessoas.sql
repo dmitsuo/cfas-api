ac8bb1db0859ca90947f2baf512c7034842018d1
Davi <davimss@gmail.com>
2019-07-16 17:49:01 -0300

    Aula 7.8 concluída

--- src/main/resources/db/migration/V002__criar_e_registrar_pessoas.sql
+++ src/main/resources/db/migration/V002__criar_e_registrar_pessoas.sql
@@ -1,15 +1,14 @@
 CREATE TABLE pessoa (
 	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50) NOT NULL,
-	naturalidade VARCHAR(50) NOT NULL,
-	ativo boolean NOT NULL default true,
-	logradouro VARCHAR(255),
-	numero VARCHAR(255),
-	complemento VARCHAR(255),
-	bairro VARCHAR(255),
-	cep VARCHAR(255),
-	cidade VARCHAR(255),
-	estado VARCHAR(255)
+	logradouro VARCHAR(30),
+	numero VARCHAR(30),
+	complemento VARCHAR(30),
+	bairro VARCHAR(30),
+	cep VARCHAR(30),
+	cidade VARCHAR(30),
+	estado VARCHAR(30),
+	ativo BOOLEAN NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('João Silva', 'Rua do Abacaxi', '10', null, 'Brasil', '38.400-121', 'Uberlândia', 'MG', true);
