fc442de65c1e9b54439ef6884bbdf9c0f453d4a8
Davi <davimss@gmail.com>
2019-07-16 17:46:26 -0300

    Aula 7.8 concluída

--- src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -35,10 +38,10 @@ public class PessoaResource {
 	@Autowired
 	private PessoaService pessoaService;
 	
-	@GetMapping
-	public List<Pessoa> listar() {	
-		return pessoaRepository.findAll();
-	}
+//	@GetMapping
+//	public List<Pessoa> listar() {	
+//		return pessoaRepository.findAll();
+//	}
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
@@ -76,7 +79,12 @@ public class PessoaResource {
 	@PutMapping("/{codigo}/ativo")
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
 	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
-        pessoaService.atualizarPropriedadeAtivo(codigo, ativo);			
-		
+        pessoaService.atualizarPropriedadeAtivo(codigo, ativo);		
 	}
+	
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+	public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "%") String nome, Pageable pageable) {
+		return pessoaRepository.findByNomeContaining(nome, pageable);
+	}	
 }
